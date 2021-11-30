d5a0bc52c77416d49aaca18de27f3702de4d924d
Jatin Koria <jkoria@rhythmone.com>
2021-11-30 15:12:06 +0530

    Generating prod config toml file

--- app/config.py
+++ app/config.py
@@ -31,4 +31,23 @@ class Config(BaseModel):
 #     config = Config.parse_obj(yaml.safe_load(config_file))
 config = Config.parse_obj(toml.load("config.toml"))
 if os.environ["CURRENT_ENV"] == "PROD":
+    cfgtoml = {}
+
+    cfgtoml["APP_NAME"] = os.environ["APP_NAME"]
+    cfgtoml["API_HEADER_PASSWORD"] = os.environ["API_HEADER_PASSWORD"]
+
+    cfgtoml["POSTGRESQL_SETTINGS"] = {}
+    cfgtoml["POSTGRESQL_SETTINGS"]["HOST"] = os.environ["POSTGRESQL_SETTINGS_HOST"]
+    cfgtoml["POSTGRESQL_SETTINGS"]["PORT"] = os.environ["POSTGRESQL_SETTINGS_PORT"]
+    cfgtoml["POSTGRESQL_SETTINGS"]["USERNAME"] = os.environ["POSTGRESQL_SETTINGS_USERNAME"]
+    cfgtoml["POSTGRESQL_SETTINGS"]["PASSWORD"] = os.environ["POSTGRESQL_SETTINGS_PASSWORD"]
+    cfgtoml["POSTGRESQL_SETTINGS"]["DATABASE"] = os.environ["POSTGRESQL_SETTINGS_DATABASE"]
+
+    cfgtoml["AUTH2_SETTINGS"] = {}
+    cfgtoml["AUTH2_SETTINGS"]["SECRET_KEY"] = os.environ["AUTH2_SETTINGS_SECRET_KEY"]
+    cfgtoml["AUTH2_SETTINGS"]["ALGORITHM"] = os.environ["AUTH2_SETTINGS_ALGORITHM"]
+    cfgtoml["AUTH2_SETTINGS"]["ACCESS_TOKEN_EXPIRE_MINUTES"] = os.environ["AUTH2_SETTINGS_ACCESS_TOKEN_EXPIRE_MINUTES"]
+
+    with open("config_prod.toml", "w") as f:
+        toml.dump(cfgtoml, f, sort_keys=False)
     config = Config.parse_obj(toml.load("config_prod.toml"))
