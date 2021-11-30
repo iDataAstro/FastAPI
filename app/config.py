13073293b923dc38044d5ef59f09ea81a0227ce2
Jatin Koria <jkoria@rhythmone.com>
2021-11-30 13:03:22 +0530

    Added config_prod.toml for prod

--- app/config.py
+++ app/config.py
@@ -29,17 +29,6 @@ class Config(BaseModel):
 
 # with open("app/config.yaml") as config_file:
 #     config = Config.parse_obj(yaml.safe_load(config_file))
-config = {}
-if os.environ["CURRENT_ENV"] == None:
-    config = Config.parse_obj(toml.load("config.toml"))
-else:
-    config["APP_NAME"] = os.environ["APP_NAME"]
-    config["API_HEADER_PASSWORD"] = os.environ["API_HEADER_PASSWORD"]
-    config["POSTGRESQL_SETTINGS"]["HOST"] = os.environ["POSTGRESQL_SETTINGS_HOST"]
-    config["POSTGRESQL_SETTINGS"]["PORT"] = os.environ["POSTGRESQL_SETTINGS_PORT"]
-    config["POSTGRESQL_SETTINGS"]["USERNAME"] = os.environ["POSTGRESQL_SETTINGS_USERNAME"]
-    config["POSTGRESQL_SETTINGS"]["PASSWORD"] = os.environ["POSTGRESQL_SETTINGS_PASSWORD"]
-    config["POSTGRESQL_SETTINGS"]["DATABASE"] = os.environ["POSTGRESQL_SETTINGS_DATABASE"]
-    config["AUTH2_SETTINGS"]["SECRET_KEY"] = os.environ["AUTH2_SETTINGS_SECRET_KEY"]
-    config["AUTH2_SETTINGS"]["ALGORITHM"] = os.environ["AUTH2_SETTINGS_ALGORITHM"]
-    config["AUTH2_SETTINGS"]["ACCESS_TOKEN_EXPIRE_MINUTES"] = os.environ["AUTH2_SETTINGS_ACCESS_TOKEN_EXPIRE_MINUTES"]
+config = Config.parse_obj(toml.load("config.toml"))
+if os.environ["CURRENT_ENV"] == "PROD":
+    config = Config.parse_obj(toml.load("config_prod.toml"))
